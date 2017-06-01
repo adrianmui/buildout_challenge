288b1818c5dbe444d6a5ece7ef875fd2bfcc827a
adrian mui <adrian.mui@gmail.com>
2017-06-01 03:48:42 -0500

    added server.js and express to prepare for deployment

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Run the app by serving the static files
+// in the dist directory
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+    return function(req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+
+app.use(express.static(__dirname + '/dist'));
+
+app.use(forceSSL());
+// Start the app by listening on the default
+// Heroku port
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8081);
